3c06f137ce43da7a3c161b1db93999727d2925bb
Brian Rozmierski <brianr@warped-minds.com>
2019-05-22 13:52:21 -0400

    Python3 Prep/Cleanup - simple print statements

--- bootstrap.py
+++ bootstrap.py
@@ -225,8 +225,8 @@ def main(key, url):
     else:
         remote_api_stub.ConfigureRemoteApiForOAuth(url, '/_ah/remote_api')
 
-    print "Loading data from The Blue Alliance requires an API key"
-    print "Please go to https://www.thebluealliance.com/account and generate a read API key"
+    print ("Loading data from The Blue Alliance requires an API key")
+    print ("Please go to https://www.thebluealliance.com/account and generate a read API key")
     apiv3_key = raw_input("Enter your API key: ")
 
     global AUTH_TOKEN
